1d1c1b5cbb0b83b599700a7031161c87563b0e6f
kerberos-doge <346137336@qq.com>
2018-05-10 16:42:48 +0800

    2.优化输出

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,11 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		println("hello");
+	}
+
+	public static void println(String string) {
+		System.out.println(string);
 	}
 
 }
